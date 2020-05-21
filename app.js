85784cf0c71bb923e675f518ab944a2f6dbfa25d
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-21 16:23:13 -0400

    Remaining hours decreases by number entered in category field

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('it works!');
+const remainingHoursSpan = document.querySelector('.remainingHours');
+const categoryField = document.querySelector('input[name="category"]');
+const TOTHOURS = 24;
+
+function handleKeyEvent(e) {
+  const categoryHours = Number(categoryField.value);
+  remainingHoursSpan.innerText = `${TOTHOURS - categoryHours}` 
+}
+
+categoryField.addEventListener('keyup', handleKeyEvent);
