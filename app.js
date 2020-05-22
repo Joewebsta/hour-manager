305dcaf21ba934cbafb26f5346a44e2f27b6de17
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-22 17:57:16 -0400

    Calculate remaining hours based on multiple inputs

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const remainingHoursSpan = document.querySelector('.remainingHours');
-const categoryField = document.querySelector('input[name="category"]');
+const hourForm = document.querySelector('#hour-form')
+const remainingHoursSpan = document.querySelector('.remaining-hours');
+const hourInputs = document.querySelectorAll('.hour-input');
 const TOTHOURS = 24;
 
+function calcTotalHours(hourFields) {
+  
+} 
+
 function handleKeyEvent(e) {
-  const categoryHours = Number(categoryField.value);
-  remainingHoursSpan.innerText = `${TOTHOURS - categoryHours}` 
+  if(!Number(e.key)) { return };
+
+  const hourInputsArr = Array.from(hourInputs);
+  const totInputHours = hourInputsArr.reduce((acc, cur) => {
+    return acc += Number(cur.value);
+  }, 0);
+
+  remainingHoursSpan.innerText = `${TOTHOURS - totInputHours}` 
+
+  // console.log(totInputHours);
+
+  // console.log(hourInputsArr);
+
+  // console.log(e.key);
+  
+  // const categoryHours = Number(categoryField.value);
+  
 }
 
-categoryField.addEventListener('keyup', handleKeyEvent);
+hourForm.addEventListener('keyup', handleKeyEvent);
